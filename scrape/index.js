7ddc7ed2741db2f656c40ddfa71ed45e6b04fb6e
Kerry Liu <kerryliu123@gmail.com>
2017-06-09 23:35:46 -0400

    2 spacings

--- scrape/index.js
+++ scrape/index.js
@@ -1,25 +1,23 @@
 const cheerio = require('cheerio');
 const http = require('http');
-
-
 /**
  * Takes a standard http function
  */
 const GET = (options) => {
-    return new Promise((resolve, reject) => {
-        let chunks = [];
-        http.get(options, (res) => {
-            res.on('data', (chunk) => {
-                chunks.push(chunk);
-            }).on('end', () => {
-                let page = Buffer.concat(chunks).toString();
-                resolve(page);
-            });
-        }).on('error', (e) => {
-            reject(e);
-        });
+  return new Promise((resolve, reject) => {
+    let chunks = [];
+    http.get(options, (res) => {
+      res.on('data', (chunk) => {
+        chunks.push(chunk);
+      }).on('end', () => {
+        let page = Buffer.concat(chunks).toString();
+        resolve(page);
+      });
+    }).on('error', (e) => {
+      reject(e);
     });
-} ;
+  });
+};
 
 module.exports.GET = GET;
 
@@ -28,6 +26,6 @@ const seed = require('./seed.json');
 
 
 GET(seed).then((page) => {
-    let $ = cheerio.load(page);
-    console.log($('table a').text());
+  let $ = cheerio.load(page);
+  console.log($('table a').text());
 });
