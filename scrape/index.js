37d8062d36750de4ee87dfdb1e52faee94a8f885
Kerry Liu <kerryliu123@gmail.com>
2017-06-09 23:27:50 -0400

    tabs to spaces pls

--- scrape/index.js
+++ scrape/index.js
@@ -6,19 +6,19 @@ const http = require('http');
  * Takes a standard http function
  */
 const GET = (options) => {
-		return new Promise((resolve, reject) => {
-				let chunks = [];
-				http.get(options, (res) => {
-						res.on('data', (chunk) => {
-								chunks.push(chunk);
-						}).on('end', () => {
+    return new Promise((resolve, reject) => {
+        let chunks = [];
+        http.get(options, (res) => {
+            res.on('data', (chunk) => {
+                chunks.push(chunk);
+            }).on('end', () => {
                 let page = Buffer.concat(chunks).toString();
-								resolve(page);
-						});
-				}).on('error', (e) => {
-						reject(e);
-				});
-		});
+                resolve(page);
+            });
+        }).on('error', (e) => {
+            reject(e);
+        });
+    });
 } ;
 
 module.exports.GET = GET;
@@ -29,5 +29,5 @@ const seed = require('./seed.json');
 
 GET(seed).then((page) => {
     let $ = cheerio.load(page);
-		console.log($('table a').text());
+    console.log($('table a').text());
 });
